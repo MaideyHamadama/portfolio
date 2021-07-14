dacc0c352ed42a20182a7eb991adfb3976146723
Maidey Hamadama <dilanechristian2@gmail.com>
2021-07-14 13:05:19 +0100

    push to heroku, change runtime

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -147,10 +147,14 @@ if os.getcwd() == '/app':
 	DATABASES = {
 		'default': dj_database_url.config(default='postgres://localhost')
     }
+    
     # Honor the 'X-Forwarded-Proto' header for request.is_secure().
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     # Allow all host headers.
+    
     ALLOWED_HOSTS = ['*']
+    
+    DEBUG = False
     # Static asset configuration
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
