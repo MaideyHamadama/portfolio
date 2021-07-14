b470e47d1b66d10a955924e462d9150fe8fe4b65
Maidey Hamadama <dilanechristian2@gmail.com>
2021-07-14 14:05:43 +0100

    Pushing to heroku. Settings no error

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #my apps
     'portfolios',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
@@ -141,23 +142,17 @@ EMAIL_USE_SSL = False
 BOOTSTRAP3 = {
 	'include_jquery': True,
 	}
+
 # Heroku settings
-if os.getcwd() == '/app':
-	import dj_database_url
-	DATABASES = {
-		'default': dj_database_url.config(default='postgres://localhost')
-    }
-    
-# Honor the 'X-Forwarded-Proto' header for request.is_secure().
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-# Allow all host headers.
-
-ALLOWED_HOSTS = ['*']
-
-DEBUG = False
-# Static asset configuration
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (
-os.path.join(BASE_DIR, 'static'),
-)
+if os.getcwd() == "/app":
+    import dj_database_url
+    DATABASES = {'default' : dj_database_url.config(default='postgres://localhost')}
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    #Allow all hosts headers
+    ALLOWED_HOSTS = ['*']
+    #Static asset configuration
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
